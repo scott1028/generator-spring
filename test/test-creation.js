ec87c25d15f70e3694109bee948690b6f93fa409
Rachel Aurand <rachel@countableset.ch>
2013-07-16 12:42:26 -0700

    fixed test

--- test/test-creation.js
+++ test/test-creation.js
@@ -4,35 +4,46 @@
 var path    = require('path');
 var helpers = require('yeoman-generator').test;
 
-
 describe('spring generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+	beforeEach(function (done) {
+		var deps = [
+			'../../app',
+			'../../controller',
+			'../../main'
+		];
+		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+			if (err) {
+				return done(err);
+			}
 
-      this.app = helpers.createGenerator('spring:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
+			this.app = helpers.createGenerator('spring:app', deps);
+			this.app.options['skip-install'] = true;
+			done();
+		}.bind(this));
+	});
 
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
-    ];
+	it('creates expected dotfiles', function (done) {
+		var expected = [
+			// add files you expect to exist here.
+			'.bowerrc'
+		];
 
-    helpers.mockPrompt(this.app, {
-      'someOption': 'Y'
-    });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFiles(expected);
-      done();
-    });
-  });
+		helpers.mockPrompt(this.app, {
+			'projectName': 'Safety Inspection',
+			'abbreviation': 'SIT',
+			'basePackage': 'edu.ucdavis.its.safetyinspection',
+			'corePackage': 'edu.ucdavis.its.core',
+			'dbUrl': '192.168.56.111',
+			'dbName': 'SIT',
+			'dbUser': 'safetyinspection',
+			'dbPassword': 'safetyinspection',
+			'siteTitle': 'Safety Inspection Tool',
+			'appname': 'safetyInspection'
+		});
+
+		this.app.run({}, function () {
+			helpers.assertFiles(expected);
+			done();
+		});
+	});
 });
