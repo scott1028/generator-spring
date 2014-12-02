7af739a7515acd36423b6f9140b3c5445848a707
Rachel Aurand <rachel@countableset.ch>
2014-12-02 10:15:56 -0800

    refactor(tests): to use helpers.run() method

--- test/test-creation.js
+++ test/test-creation.js
@@ -4,236 +4,198 @@ var path    = require('path');
 var helpers = require('yeoman-generator').test;
 var assert  = require('yeoman-generator').assert;
 
-describe('Spring Generator', function () {
-  beforeEach(function (done) {
-    var deps = [
-      '../../app'
-    ];
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
-
-      this.app = helpers.createGenerator('spring:app', deps);
-      this.app.options['skip-install'] = true;
-
-      helpers.mockPrompt(this.app, {
-        'projectName': 'Demo App',
-        'basePackage': 'ch.example.demo'
-      });
-      done();
-    }.bind(this));
-  });
-
-  it('should create expected config files', function (done) {
-    var expected = [
-      'vagrant/bootstrap.sh',
-      'bower.json',
-      '.bowerrc',
-      '.gitignore',
-      'Gruntfile.js',
-      '.jshintrc',
-      'karma.conf.js',
-      'package.json',
-      'README.md',
-      'Vagrantfile',
-      'build.gradle',
-      'settings.gradle'
-    ];
-
-    this.app.run({}, function () {
-      assert.files(expected);
-      done();
+describe('Spring Generator Test Creation', function () {
+  describe('App', function () {
+    beforeEach(function (done) {
+      helpers.run(path.join( __dirname, '../app'))
+        .withOptions({ 'skip-install': true })
+        .withPrompts({
+          'projectName': 'Demo App',
+          'basePackage': 'ch.example.demo'
+        })
+        .on('end', done);
     });
-  });
 
-  it('should create expected java package files', function (done) {
-    var expected = [
-      ['src/main/java/ch/example/demo/config/DataBaseConfig.java', /package ch\.example\.demo\.config;/],
-      ['src/main/java/ch/example/demo/config/SecurityConfig.java', /package ch\.example\.demo\.config;/],
-      ['src/main/java/ch/example/demo/config/Application.java', /package ch\.example\.demo\.config;/],
+    it('should create expected config files', function () {
+      var expected = [
+        'vagrant/bootstrap.sh',
+        'bower.json',
+        '.bowerrc',
+        '.gitignore',
+        'Gruntfile.js',
+        '.jshintrc',
+        'karma.conf.js',
+        'package.json',
+        'README.md',
+        'Vagrantfile',
+        'build.gradle',
+        'settings.gradle'
+      ];
 
-      ['src/main/java/ch/example/demo/controller/AccountController.java', /package ch\.example\.demo\.controller;/],
+      assert.file(expected);
+    });
 
-      ['src/main/java/ch/example/demo/model/AbstractEntity.java', /package ch\.example\.demo\.model;/],
-      ['src/main/java/ch/example/demo/model/Account.java', /package ch\.example\.demo\.model;/],
+    it('should create expected java package files', function () {
+      var expected = [
+        ['src/main/java/ch/example/demo/config/DataBaseConfig.java', /package ch\.example\.demo\.config;/],
+        ['src/main/java/ch/example/demo/config/SecurityConfig.java', /package ch\.example\.demo\.config;/],
+        ['src/main/java/ch/example/demo/config/Application.java', /package ch\.example\.demo\.config;/],
 
-      ['src/main/java/ch/example/demo/repository/AccountRepository.java', /package ch\.example\.demo\.repository;/],
+        ['src/main/java/ch/example/demo/controller/AccountController.java', /package ch\.example\.demo\.controller;/],
 
-      ['src/main/java/ch/example/demo/security/AjaxAuthenticationFailureHandler.java', /package ch\.example\.demo\.security;/],
-      ['src/main/java/ch/example/demo/security/AjaxAuthenticationSuccessHandler.java', /package ch\.example\.demo\.security;/],
-      ['src/main/java/ch/example/demo/security/AjaxLogoutSuccessHandler.java', /package ch\.example\.demo\.security;/],
-      ['src/main/java/ch/example/demo/security/Http401UnauthorizedEntryPoint.java', /package ch\.example\.demo\.security;/],
-      ['src/main/java/ch/example/demo/security/SpringSecurityAuditorAware.java', /package ch\.example\.demo\.security;/],
-      ['src/main/java/ch/example/demo/security/UserDetailsCustom.java', /package ch\.example\.demo\.security;/],
-      ['src/main/java/ch/example/demo/security/UserDetailsService.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/model/AbstractEntity.java', /package ch\.example\.demo\.model;/],
+        ['src/main/java/ch/example/demo/model/Account.java', /package ch\.example\.demo\.model;/],
 
-      ['src/main/java/ch/example/demo/service/AccountService.java', /package ch\.example\.demo\.service;/],
-      ['src/main/java/ch/example/demo/service/AccountServiceImpl.java', /package ch\.example\.demo\.service;/],
-      ['src/main/java/ch/example/demo/service/BaseService.java', /package ch\.example\.demo\.service;/],
-      ['src/main/java/ch/example/demo/service/BaseServiceImpl.java', /package ch\.example\.demo\.service;/]
-    ];
+        ['src/main/java/ch/example/demo/repository/AccountRepository.java', /package ch\.example\.demo\.repository;/],
 
-    this.app.run({}, function () {
+        ['src/main/java/ch/example/demo/security/AjaxAuthenticationFailureHandler.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/security/AjaxAuthenticationSuccessHandler.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/security/AjaxLogoutSuccessHandler.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/security/Http401UnauthorizedEntryPoint.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/security/SpringSecurityAuditorAware.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/security/UserDetailsCustom.java', /package ch\.example\.demo\.security;/],
+        ['src/main/java/ch/example/demo/security/UserDetailsService.java', /package ch\.example\.demo\.security;/],
+
+        ['src/main/java/ch/example/demo/service/AccountService.java', /package ch\.example\.demo\.service;/],
+        ['src/main/java/ch/example/demo/service/AccountServiceImpl.java', /package ch\.example\.demo\.service;/],
+        ['src/main/java/ch/example/demo/service/BaseService.java', /package ch\.example\.demo\.service;/],
+        ['src/main/java/ch/example/demo/service/BaseServiceImpl.java', /package ch\.example\.demo\.service;/]
+      ];
+
       assert.fileContent(expected);
-      done();
     });
-  });
 
-  it('should create junit test java files', function (done) {
-    var expected = [
-      ['src/test/java/ch/example/demo/test/config/MockAuditorAware.java', /package ch\.example\.demo\.test\.config;/],
-      ['src/test/java/ch/example/demo/test/config/TestConfig.java', /package ch\.example\.demo\.test\.config;/],
-      ['src/test/java/ch/example/demo/test/repository/BaseIntegrationTest.java', /package ch\.example\.demo\.test\.repository;/]
-    ];
+    it('should create junit test java files', function () {
+      var expected = [
+        ['src/test/java/ch/example/demo/test/config/MockAuditorAware.java', /package ch\.example\.demo\.test\.config;/],
+        ['src/test/java/ch/example/demo/test/config/TestConfig.java', /package ch\.example\.demo\.test\.config;/],
+        ['src/test/java/ch/example/demo/test/repository/BaseIntegrationTest.java', /package ch\.example\.demo\.test\.repository;/]
+      ];
 
-    this.app.run({}, function () {
       assert.fileContent(expected);
-      done();
     });
-  });
 
-  it('should create project related files', function (done) {
-    var expected = [
-      'src/main/resources/application-local.properties',
-      'src/main/resources/base-data.sql',
-      'src/main/resources/favicon.ico',
-      'karma/controllers/login/login_spec.js',
-      'karma/controllers/main/main_spec.js',
-      'karma/controllers/register/register_spec.js',
-      'karma/services/account_spec.js',
-      'karma/services/auth_spec.js',
-      'src/main/resources/public/scripts/controllers/login/login.html',
-      'src/main/resources/public/scripts/controllers/login/login.js',
-      'src/main/resources/public/scripts/controllers/main/main.html',
-      'src/main/resources/public/scripts/controllers/main/main.js',
-      'src/main/resources/public/scripts/controllers/navbar/navbar.js',
-      'src/main/resources/public/scripts/controllers/register/register.html',
-      'src/main/resources/public/scripts/controllers/register/register.js',
-      'src/main/resources/public/scripts/services/account.js',
-      'src/main/resources/public/scripts/services/auth.js',
-      'src/main/resources/public/scripts/services/interceptor.js',
-      'src/main/resources/public/scripts/app.js',
-      'src/main/resources/public/styles/master.less',
-      'src/main/resources/public/styles/mixins.less',
-      'src/main/resources/public/styles/project.less',
-      'src/main/resources/public/styles/variables.less',
-      'src/main/resources/public/index.html',
-      'src/test/resources/application-test.properties',
-      'src/test/resources/data.sql',
-      'src/test/resources/logback.xml'
-    ];
+    it('should create project related files', function () {
+      var expected = [
+        'src/main/resources/application-local.properties',
+        'src/main/resources/base-data.sql',
+        'src/main/resources/favicon.ico',
+        'karma/controllers/login/login_spec.js',
+        'karma/controllers/main/main_spec.js',
+        'karma/controllers/register/register_spec.js',
+        'karma/services/account_spec.js',
+        'karma/services/auth_spec.js',
+        'src/main/resources/public/scripts/controllers/login/login.html',
+        'src/main/resources/public/scripts/controllers/login/login.js',
+        'src/main/resources/public/scripts/controllers/main/main.html',
+        'src/main/resources/public/scripts/controllers/main/main.js',
+        'src/main/resources/public/scripts/controllers/navbar/navbar.js',
+        'src/main/resources/public/scripts/controllers/register/register.html',
+        'src/main/resources/public/scripts/controllers/register/register.js',
+        'src/main/resources/public/scripts/services/account.js',
+        'src/main/resources/public/scripts/services/auth.js',
+        'src/main/resources/public/scripts/services/interceptor.js',
+        'src/main/resources/public/scripts/app.js',
+        'src/main/resources/public/styles/master.less',
+        'src/main/resources/public/styles/mixins.less',
+        'src/main/resources/public/styles/project.less',
+        'src/main/resources/public/styles/variables.less',
+        'src/main/resources/public/index.html',
+        'src/test/resources/application-test.properties',
+        'src/test/resources/data.sql',
+        'src/test/resources/logback.xml'
+      ];
 
-    this.app.run({}, function () {
-      assert.files(expected);
-      done();
+      assert.file(expected);
     });
   });
 
   describe('Controller', function () {
-    it('should generate a new controller', function (done) {
-      var deps = ['../../controller'];
-      var controller = helpers.createGenerator('spring:controller', deps, ['foo']);
-      var expected = [
-        ['src/main/resources/public/scripts/controllers/foo/foo.js', /controller\('FooCtrl'/],
-        ['src/main/resources/public/scripts/controllers/foo/foo.html', / /],
-        ['karma/spec/controllers/foo/foo.js', /describe\('Controller: FooCtrl'/],
-        ['src/main/resources/public/index.html', /script src\="resources\/scripts\/controllers\/foo\/foo.js"/]
-      ];
-      this.app.run({}, function () {
-        controller.run({}, function () {
+    it('should generate a new controller', function () {
+      helpers.run(path.join( __dirname, '../controller'))
+        .withArguments(['foo'])
+        .on('end', function () {
+          var expected = [
+            ['src/main/resources/public/scripts/controllers/foo/foo.js', /controller\('FooCtrl'/],
+            ['src/main/resources/public/scripts/controllers/foo/foo.html', / /],
+            ['karma/spec/controllers/foo/foo.js', /describe\('Controller: FooCtrl'/],
+            ['src/main/resources/public/index.html', /script src\="resources\/scripts\/controllers\/foo\/foo.js"/]
+          ];
           assert.fileContent(expected);
-          done();
         });
-      });
     });
 
-    it('should generate a new controller with correct structure', function (done) {
-      var deps = ['../../controller'];
-      var controller = helpers.createGenerator('spring:controller', deps, ['foo/bar']);
-      var expected = [
-        ['src/main/resources/public/scripts/controllers/foo/bar/bar.js', /controller\('FooBarCtrl'/],
-        ['src/main/resources/public/scripts/controllers/foo/bar/bar.html', / /],
-        ['karma/spec/controllers/foo/bar/bar.js', /describe\('Controller: FooBarCtrl'/],
-        ['src/main/resources/public/index.html', /script src\="resources\/scripts\/controllers\/foo\/bar\/bar.js"/]
-      ];
-      this.app.run({}, function () {
-        controller.run({}, function () {
+    it('should generate a new controller with correct structure', function () {
+      helpers.run(path.join( __dirname, '../controller'))
+        .withArguments(['foo/bar'])
+        .on('end', function () {
+          var expected = [
+            ['src/main/resources/public/scripts/controllers/foo/bar/bar.js', /controller\('FooBarCtrl'/],
+            ['src/main/resources/public/scripts/controllers/foo/bar/bar.html', / /],
+            ['karma/spec/controllers/foo/bar/bar.js', /describe\('Controller: FooBarCtrl'/],
+            ['src/main/resources/public/index.html', /script src\="resources\/scripts\/controllers\/foo\/bar\/bar.js"/]
+          ];
           assert.fileContent(expected);
-          done();
         });
-      });
     });
 
-    it('should generate a new controller without leading and trailing slashes', function (done) {
-      var deps = ['../../controller'];
-      var controller = helpers.createGenerator('spring:controller', deps, ['/foo/bar/']);
-      var expected = [
-        ['src/main/resources/public/scripts/controllers/foo/bar/bar.js', /controller\('FooBarCtrl'/],
-        ['src/main/resources/public/scripts/controllers/foo/bar/bar.html', / /],
-        ['karma/spec/controllers/foo/bar/bar.js', /describe\('Controller: FooBarCtrl'/],
-        ['src/main/resources/public/index.html', /script src\="resources\/scripts\/controllers\/foo\/bar\/bar.js"/]
-      ];
-      this.app.run({}, function () {
-        controller.run({}, function () {
+    it('should generate a new controller without leading and trailing slashes', function () {
+      helpers.run(path.join( __dirname, '../controller'))
+        .withArguments(['/foo/bar/'])
+        .on('end', function () {
+          var expected = [
+            ['src/main/resources/public/scripts/controllers/foo/bar/bar.js', /controller\('FooBarCtrl'/],
+            ['src/main/resources/public/scripts/controllers/foo/bar/bar.html', / /],
+            ['karma/spec/controllers/foo/bar/bar.js', /describe\('Controller: FooBarCtrl'/],
+            ['src/main/resources/public/index.html', /script src\="resources\/scripts\/controllers\/foo\/bar\/bar.js"/]
+          ];
           assert.fileContent(expected);
-          done();
         });
-      });
     });
   });
 
   describe('Directive', function () {
-    it('should generate a new directive', function (done) {
-      var deps = ['../../directive'];
-      var directive = helpers.createGenerator('spring:directive', deps, ['foo']);
-      var expected = [
-        ['src/main/resources/public/scripts/directives/foo.js', /directive\('foo'/],
-        ['karma/spec/directives/foo.js', /describe\('Directive: foo'/],
-        ['src/main/resources/public/index.html', /script src\="resources\/scripts\/directives\/foo.js"/]
-      ];
-      this.app.run({}, function () {
-        directive.run({}, function () {
+    it('should generate a new directive', function () {
+      helpers.run(path.join( __dirname, '../directive'))
+        .withArguments(['foo'])
+        .on('end', function () {
+          var expected = [
+            ['src/main/resources/public/scripts/directives/foo.js', /directive\('foo'/],
+            ['karma/spec/directives/foo.js', /describe\('Directive: foo'/],
+            ['src/main/resources/public/index.html', /script src\="resources\/scripts\/directives\/foo.js"/]
+          ];
           assert.fileContent(expected);
-          done();
         });
-      });
     });
   });
 
   describe('Filter', function () {
-    it('should generate a new filter', function (done) {
-      var deps = ['../../filter'];
-      var filter = helpers.createGenerator('spring:filter', deps, ['foo']);
-      var expected = [
-        ['src/main/resources/public/scripts/filters/foo.js', /filter\('foo'/],
-        ['karma/spec/filters/foo.js', /describe\('Filter: foo'/],
-        ['src/main/resources/public/index.html', /script src\="resources\/scripts\/filters\/foo.js"/]
-      ];
-      this.app.run({}, function () {
-        filter.run({}, function () {
+    it('should generate a new filter', function () {
+      helpers.run(path.join( __dirname, '../filter'))
+        .withArguments(['foo'])
+        .on('end', function () {
+          var expected = [
+            ['src/main/resources/public/scripts/filters/foo.js', /filter\('foo'/],
+            ['karma/spec/filters/foo.js', /describe\('Filter: foo'/],
+            ['src/main/resources/public/index.html', /script src\="resources\/scripts\/filters\/foo.js"/]
+          ];
           assert.fileContent(expected);
-          done();
         });
-      });
     });
   });
 
   describe('Service', function () {
-    it('should generate a new service', function (done) {
-      var deps = ['../../service'];
-      var service = helpers.createGenerator('spring:service', deps, ['foo']);
-      var expected = [
-        ['src/main/resources/public/scripts/services/foo.js', /factory\('FooService'/],
-        ['karma/spec/services/foo.js', /describe\('Service: FooService'/],
-        ['src/main/resources/public/index.html', /script src\="resources\/scripts\/services\/foo.js"/]
-      ];
-      this.app.run({}, function () {
-        service.run({}, function () {
+    it('should generate a new service', function () {
+      helpers.run(path.join( __dirname, '../service'))
+        .withArguments(['foo'])
+        .on('end', function () {
+          var expected = [
+            ['src/main/resources/public/scripts/services/foo.js', /factory\('FooService'/],
+            ['karma/spec/services/foo.js', /describe\('Service: FooService'/],
+            ['src/main/resources/public/index.html', /script src\="resources\/scripts\/services\/foo.js"/]
+          ];
           assert.fileContent(expected);
-          done();
         });
-      });
     });
   });
 });
--- test/test-load.js
+++ test/test-load.js
@@ -3,9 +3,17 @@
 
 var assert  = require('assert');
 
-describe('spring generator', function () {
+describe('Spring Generator Test Load', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
+    var controller = require('../controller');
+    // var directive = require('../directive');
+    var filter = require('../filter');
+    var service = require('../service');
     assert(app !== undefined);
+    assert(controller !== undefined);
+    // assert(directive !== undefined);
+    assert(filter !== undefined);
+    assert(service !== undefined);
   });
 });
