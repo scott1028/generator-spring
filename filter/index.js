03f7b707ed78bf4d1d5182945de24d1b837b63b4
Rachel Aurand <rachel@countableset.ch>
2014-12-02 08:09:39 -0800

    refactor(filter): update to new generator style

--- filter/index.js
+++ filter/index.js
@@ -1,18 +1,17 @@
 'use strict';
 var path = require('path');
 var util = require('util');
-var ScriptBase = require('../script-base.js');
+var yeoman = require('yeoman-generator');
+var genUtil = require('../util.js');
 
-function Generator() {
-  ScriptBase.apply(this, arguments);
-}
+var FilterGenerator = yeoman.generators.NamedBase.extend({
+  writing: function () {
+    this.template('_filter.js',
+        path.join(genUtil.resourcePath(), 'scripts/filters/', this.name + '.js'));
+    this.template('_spec.js',
+        path.join(genUtil.testPath(), 'spec/filters/', this.name + '.js'));
+    genUtil.addScriptToIndex('resources/scripts/filters/' + this.name);
+  }
+});
 
-module.exports = Generator;
-
-util.inherits(Generator, ScriptBase);
-
-Generator.prototype.createFilterFiles = function createFilterFiles() {
-  this.template('_filter.js', path.join(this.resourcePath(), 'scripts/filters/', this.name + '.js'));
-  this.template('_spec.js', path.join(this.testPath(), 'spec/filters/', this.name + '.js'));
-  this.addScriptToIndex('resources/scripts/filters/' + this.name);
-};
+module.exports = FilterGenerator;
