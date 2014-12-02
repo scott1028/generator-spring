7971b170bcbfecbcce83688f8ff3053baef83cff
Rachel Aurand <rachel@countableset.ch>
2014-12-02 08:07:45 -0800

    refactor(directive): update to new generator style

--- directive/index.js
+++ directive/index.js
@@ -1,18 +1,17 @@
 'use strict';
 var path = require('path');
 var util = require('util');
-var ScriptBase = require('../script-base.js');
+var yeoman = require('yeoman-generator');
+var genUtil = require('../util.js');
 
-function Generator() {
-  ScriptBase.apply(this, arguments);
-}
+var DirectiveGenerator = yeoman.generators.NamedBase.extend({
+  writing: function () {
+    this.template('_directive.js',
+        path.join(genUtil.resourcePath(), 'scripts/directives', this.name + '.js'));
+    this.template('_spec.js',
+        path.join(genUtil.testPath(), 'spec/directives', this.name + '.js'));
+    genUtil.addScriptToIndex('resources/scripts/directives/' + this.name);
+  }
+});
 
-module.exports = Generator;
-
-util.inherits(Generator, ScriptBase);
-
-Generator.prototype.createDirectiveFiles = function createDirectiveFiles() {
-  this.template('_directive.js', path.join(this.resourcePath(), 'scripts/directives', this.name + '.js'));
-  this.template('_spec.js', path.join(this.testPath(), 'spec/directives', this.name + '.js'));
-  this.addScriptToIndex('resources/scripts/directives/' + this.name);
-};
+module.exports = DirectiveGenerator;
